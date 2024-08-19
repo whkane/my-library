24b99fdff63934c36113393085f3eaa39f6f2f78
John Doe <johndoe@email.com>
2024-08-19 21:44:07 +0300

    Most of the functionality ready

--- my_library_db.py
+++ my_library_db.py
@@ -19,7 +19,12 @@ class MyLibraryApp:
         self.publish_txt = self.create_entry_field(root, "Publishing year", 10, 25, 15, 25, 50)
 
         def on_click_add_book():
-            messagebox.askquestion("Confirm","Are you sure you want to add a book?")
+            answer = messagebox.askokcancel(title='Confirmation', message='Are you sure you want to add a book?')
+            if answer:
+                self.add_book(sys.argv[1])
+                messagebox.showinfo("Add book", "New book added successfully!")
+            else:
+                messagebox.showinfo("Add book", "No new books added.")
         # Create button to add a book to the library
         self.btn_add_book = self.create_button(root, "Add Book", "green", on_click_add_book, 20, 15, 150)
 
@@ -32,6 +37,19 @@ class MyLibraryApp:
             self.print_library_content(sys.argv[1])
         # Create button to print current library content
         self.btn_print = self.create_button(root, "Display Library Content", "blue", on_click_display, 20, 15, 175)
+
+    def add_book(self, json_file):
+        # Get values from entry fields and append them to JSON file
+        with open(json_file, 'r+') as file:
+            books=json.load(file)
+            new_book = self.book_txt.get()
+            new_author = self.author_txt.get()
+            new_isbn = self.isbn_txt.get()
+            new_publish = self.publish_txt.get()
+            new_book_obj = {'title':new_book,'author':new_author,'isbn':new_isbn,'year':new_publish}
+            books['library'].append(new_book_obj)
+            file.seek(0)
+            json.dump(books, file, indent=4)
 
     def create_entry_field(self, root, type, l_col, l_row, t_col, t_row, t_width):
         lbl = Label(root, text=type)
